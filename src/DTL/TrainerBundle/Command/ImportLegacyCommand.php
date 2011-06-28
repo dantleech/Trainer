8dde83d2a6a47a4b82d4bdfe2ae9dd73ec03be22
ds <dan.t.leech@gmail.com>
2011-06-28 23:52:33 +0100

    Note function, basic authentication

--- src/DTL/TrainerBundle/Command/ImportLegacyCommand.php
+++ src/DTL/TrainerBundle/Command/ImportLegacyCommand.php
@@ -7,11 +7,11 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\Output;
-use Symfony\Bundle\FrameworkBundle\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use DTL\TrainerBundle\Document\Route;
 use DTL\TrainerBundle\Document\Session;
 
-class ImportLegacyCommand extends Command
+class ImportLegacyCommand extends ContainerAwareCommand
 {
     /**
      * @see Command
