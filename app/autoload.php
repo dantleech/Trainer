2e86381e3358a551fecb296fd2069a0c1fac978f
dantleech <dan.t.leech@gmail.com>
2012-01-02 23:15:04 +0000

    Updated deps

--- app/autoload.php
+++ app/autoload.php
@@ -7,6 +7,7 @@ $loader->registerNamespaces(array(
     'Sensio'                        => __DIR__.'/../vendor/bundles',
     'JMS'                           => __DIR__.'/../vendor/bundles',
     'FOS'                           => __DIR__.'/../vendor/bundles',
+    'Doctrine\\Bundle'              => __DIR__.'/../vendor/bundles',
     'Doctrine\\Common'              => __DIR__.'/../vendor/doctrine-common/lib',
     'Doctrine\\Common\\DataFixtures'=> __DIR__.'/../vendor/doctrine-fixtures/lib',
     'Doctrine\\DBAL'                => __DIR__.'/../vendor/doctrine-dbal/lib',
